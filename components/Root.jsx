66befa9044db17fdd64e94b20e66cf4d23e44e44
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-11-05 01:14:23 +0530

    Added Radium

--- components/Root.jsx
+++ components/Root.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import Radium from 'radium';
 
 class Root extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const styles = {
+            color: '#000',
+            ':hover': {
+                backgroundColor: '#0074d9'
+            }
+        };
         return(
-            <p>Hello World!</p>
+            <div>
+                <p style={{color: 'blue'}} >Hello World!</p>
+                <button style={styles}>Button</button>
+            </div>
         );
     }
 }
 
-module.exports = Root;
+module.exports = Radium(Root);
